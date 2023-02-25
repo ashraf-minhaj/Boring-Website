5f94bcc2f942c45d2729c4a197093519834ae0b6
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2023-02-26 01:43:45 +0600

    bucket policy

--- Terraform/bucket.tf
+++ Terraform/bucket.tf
@@ -2,6 +2,41 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket            = "${var.bucket_name}"   
 }
 
+resource "aws_s3_bucket_policy" "destination_bucket_policy" {
+  bucket = "${aws_s3_bucket.s3_bucket.id}"
+  policy = jsonencode(
+      {
+        "Version": "2008-10-17",
+        "Id": "PolicyForCloudFrontPrivateContent",
+        "Statement": [
+          {
+            "Sid": "Allow our access key",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": "s3:*",
+            "Resource": "arn:aws:s3:::${var.bucket_name}/*"
+          },
+          {
+            "Sid": "AllowCloudFrontServicePrincipal",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "cloudfront.amazonaws.com"
+              },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${var.bucket_name}/*",
+            "Condition": {
+              "StringEquals": {
+                  "AWS:SourceArn": "${aws_cloudfront_distribution.cdn.arn}"
+            }
+          }
+        }
+      ]
+    }
+  )
+}
+
 # making the s3 bucket private 
 resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   bucket            = aws_s3_bucket.s3_bucket.id
